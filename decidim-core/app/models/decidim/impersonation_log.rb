a8153605cca3a28c2be1e10201d3dfb88debd8b8
Marc Riera Casals <mrc2407@gmail.com>
2021-01-26 11:23:34 +0100

    Reduce SESSION_TIME_IN_MINUTES to 1

--- decidim-core/app/models/decidim/impersonation_log.rb
+++ decidim-core/app/models/decidim/impersonation_log.rb
@@ -3,12 +3,12 @@
 module Decidim
   # ImpersonationLogs are created whenever an admin impersonate a managed user
   class ImpersonationLog < ApplicationRecord
-    SESSION_TIME_IN_MINUTES = 30
+    SESSION_TIME_IN_MINUTES = 1
 
     belongs_to :admin, foreign_key: "decidim_admin_id", class_name: "Decidim::User"
     belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
 
-    validate :same_organization
+    validate :same_organization, :non_active_impersonation
 
     scope :active, -> { where(ended_at: nil, expired_at: nil) }
     scope :expired, -> { where(ended_at: nil).where.not(expired_at: nil) }
@@ -21,16 +21,18 @@ module Decidim
       expired_at.present?
     end
 
-    def self.log_presenter_class_for(_log)
-      Decidim::AdminLog::ImpersonationLogPresenter
-    end
-
     private
 
     def same_organization
       return if admin&.organization == user&.organization
 
       errors.add(:admin, :invalid)
+    end
+
+    def non_active_impersonation
+      return if ended? || expired?
+
+      errors.add(:admin, :invalid) if Decidim::ImpersonationLog.where(admin: admin).active.any?
     end
   end
 end
